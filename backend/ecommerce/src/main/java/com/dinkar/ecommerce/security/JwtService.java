bf40a8233c43948287fac069b77b910f6d18ff54
Dinkar Upadhyay <dinkarupadhyay2906@gmail.com>
2026-07-20 23:26:14 +0530

    jwt service

---
+++ backend/ecommerce/src/main/java/com/dinkar/ecommerce/security/JwtService.java
@@ -0,0 +1,46 @@
+package com.dinkar.ecommerce.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET =
+            "ThisIsMyVerySecretKeyForJwtAuthenticationProject2026";
+
+    private SecretKey getSigningKey() {
+
+        return Keys.hmacShaKeyFor(
+                SECRET.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+
+                .subject(email)
+
+                .issuedAt(new Date())
+
+                .expiration(
+                        new Date(
+                                System.currentTimeMillis()
+                                        + 1000 * 60 * 60
+                        )
+                )
+
+                .signWith(getSigningKey())
+
+                .compact();
+    }
+
+}
